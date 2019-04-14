b49f41c9b3d5759e9b391be93429180b870fcfc6
Jangni <90706827@163.com>
2019-04-15 00:21:46 +0800

    guava

--- src/main/java/com/jangni/guava/async/ListenableFutureDemo.java
+++ src/main/java/com/jangni/guava/async/ListenableFutureDemo.java
@@ -1,10 +1,14 @@
 package com.jangni.guava.async;
 
 import com.google.common.util.concurrent.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
@@ -15,104 +19,65 @@ import java.util.concurrent.Executors;
  * @Version 1.0
  **/
 public class ListenableFutureDemo {
+    static final Logger logger = LoggerFactory.getLogger(ListenableFutureDemo.class);
+
+    static final ExecutorService executor = Executors.newFixedThreadPool(50);
     // 创建线程池
-    final static ListeningExecutorService service = MoreExecutors.listeningDecorator(Executors.newCachedThreadPool());
+    static final ListeningExecutorService service = MoreExecutors.listeningDecorator(executor);
+
+    final List<String> value = Collections.synchronizedList(new ArrayList<String>());
 
     public static void main(String[] args) throws Exception {
-
-        long t1 = System.currentTimeMillis();
-
-        // 任务1
-        ListenableFuture<Boolean> booleanTask = service.submit(() -> true);
-
-        Futures.addCallback(booleanTask, new FutureCallback<Boolean>() {
-            @Override
-            public void onSuccess(Boolean result) {
-                System.err.println("BooleanTask: " + result);
-            }
-
-            @Override
-            public void onFailure(Throwable t) {
-
-            }
-        }, service);
-
-        // 任务2
-        ListenableFuture<String> stringTask = service.submit(() -> "Hello World");
-
-        Futures.addCallback(stringTask, new FutureCallback<String>() {
-            @Override
-            public void onSuccess(String result) {
-                System.err.println("StringTask: " + result);
-            }
-
-            @Override
-            public void onFailure(Throwable t) {
-            }
-        }, service);
-
-        // 任务3
-        ListenableFuture<Integer> integerTask = service.submit(() -> new Random().nextInt(100));
-
-        Futures.addCallback(integerTask, new FutureCallback<Integer>() {
-            @Override
-            public void onSuccess(Integer result) {
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        final List<String> value = Collections
+                .synchronizedList(new ArrayList<String>());
+        try {
+            List<ListenableFuture<String>> futures = new ArrayList<ListenableFuture<String>>();
+            // 将实现了callable的任务放入到线程池中，得到一个带有回调机制的ListenableFuture实例，
+            // 通过Futures.addCallback方法对得到的ListenableFuture实例进行监听，一旦得到结果就进入到onSuccess方法中，
+            // 在onSuccess方法中将查询的结果存入到集合中
+            for (int i = 0; i < 10; i++) {
+                final int index = i;
+                if (i == 9) {
+                    Thread.sleep(500 * i);
                 }
-                System.err.println("IntegerTask: " + result);
+                ListenableFuture<String> sfuture = service
+                        .submit(() -> {
+                            long time = System.currentTimeMillis();
+                            logger.info("Finishing sleeping task{}: {}", index, time);
+                            return String.valueOf(time);
+                        });
+
+                sfuture.addListener(new Runnable() {
+                    @Override
+                    public void run() {
+                        logger.info("Listener be triggered for task{}.", index);
+                    }
+                }, service);
+
+                Futures.addCallback(sfuture, new FutureCallback<String>() {
+                    public void onSuccess(String result) {
+                        logger.info("Add result value into value list {}.", result);
+                        value.add(result);
+                    }
+
+                    public void onFailure(Throwable t) {
+                        logger.info("Add result value into value list error.", t);
+                        throw new RuntimeException(t);
+                    }
+                },service);
+                // 将每一次查询得到的ListenableFuture放入到集合中
+                futures.add(sfuture);
             }
 
-            @Override
-            public void onFailure(Throwable t) {
-            }
-        }, service);
-
-        // 执行时间
-        System.err.println("time: " + (System.currentTimeMillis() - t1));
-
-
-        ListenableFuture future1 = service.submit(new Callable<Integer>() {
-            public Integer call() throws InterruptedException {
-                Thread.sleep(1000);
-                System.out.println("call future 1.");
-                return 1;
-            }
-        });
-
-        ListenableFuture future2 = service.submit(new Callable<Integer>() {
-            public Integer call() throws InterruptedException {
-                Thread.sleep(1000);
-                System.out.println("call future 2.");
-                //    throw new RuntimeException("----call future 2.");
-                return 2;
-            }
-        });
-
-        final ListenableFuture allFutures = Futures.allAsList(future1, future2);
-
-        final ListenableFuture transform = Futures.transformAsync(allFutures, new AsyncFunction<List<Integer>, Boolean>() {
-            @Override
-            public ListenableFuture apply(List<Integer> results) throws Exception {
-                return Futures.immediateFuture(String.format("success future:%d", results.size()));
-            }
-        }, service);
-
-        Futures.addCallback(transform, new FutureCallback<Object>() {
-
-            public void onSuccess(Object result) {
-                System.out.println(result.getClass());
-                System.out.printf("success with: %s%n", result);
-            }
-
-            public void onFailure(Throwable thrown) {
-                System.out.printf("onFailure%s%n", thrown.getMessage());
-            }
-        }, service);
-
-        System.out.println(transform.get());
+            // 这里将集合中的若干ListenableFuture形成一个新的ListenableFuture
+            // 目的是为了异步阻塞，直到所有的ListenableFuture都得到结果才继续当前线程
+            // 这里的时间取的是所有任务中用时最长的一个
+            ListenableFuture<List<String>> allAsList = Futures.allAsList(futures);
+            allAsList.get();
+            logger.info("All sub-task are finished.");
+        } catch (Exception ignored) {
+            ignored.getMessage();
+        }
     }
 
 }
---
+++ src/main/java/com/jangni/guava/async/linkFutureDemo.java
@@ -0,0 +1,64 @@
+package com.jangni.guava.async;
+
+import com.google.common.util.concurrent.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.concurrent.DefaultManagedAwareThreadFactory;
+
+import java.util.concurrent.*;
+
+/**
+ * @ClassName linkFutureDemo
+ * @Description 链式回调
+ * @Author Mr.Jangni
+ * @Date 2019/4/14 21:03
+ * @Version 1.0
+ **/
+public class linkFutureDemo {
+    static final ExecutorService executor = Executors.newFixedThreadPool(50);
+    static final Logger logger = LoggerFactory.getLogger(ListenableFutureDemo.class);
+    // 创建线程池
+    static final ListeningExecutorService service = MoreExecutors.listeningDecorator(executor);
+
+
+    public static void main(String[] args) {
+
+        ListenableFuture<String> start = service.submit(() -> {
+            logger.info("任务执行开始...");
+            return "start";
+        });
+
+        ListenableFuture<String> one = Futures.transformAsync(start, s -> service.submit(() -> {
+            logger.info(s);
+            return "one";
+        }), service);
+
+
+        ListenableFuture<String> two = Futures.transformAsync(one, s -> service.submit(() -> {
+            logger.info(s);
+            return "two";
+        }), service);
+
+        ListenableFuture<String> three = Futures.transformAsync(two, s -> service.submit(() -> {
+            logger.info(s);
+            return "three";
+        }), service);
+
+        ListenableFuture<String> four = Futures.transformAsync(three, s -> service.submit(() -> {
+            logger.info(s);
+            return "four";
+        }), service);
+
+        Futures.addCallback(four, new FutureCallback<String>() {
+            public void onSuccess(String result) {
+                logger.info(result);
+                logger.info("任务执行完成");
+            }
+
+            public void onFailure(Throwable t) {
+
+                logger.error("异常",t);
+            }
+        }, service);
+    }
+}
